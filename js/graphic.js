fd44fb819920ba1d8d86b519478205f4d3447754
scottpham <scott.pham85@gmail.com>
2016-02-29 15:47:11 -0800

    fixed resizing issue, better ordering of libraries

--- js/graphic.js
+++ js/graphic.js
@@ -1,50 +1,59 @@
 //Edit 'key' and 'columns' to connect your spreadsheet
 
 //enter google sheets key here
-var key = "https://docs.google.com/spreadsheets/d/113QZOTS9JQ1WGfwjwQrOpErUBJTAnaBmdjP0_OjA53c/pubhtml?gid=489792061&single=true";
+var key =
+  "https://docs.google.com/spreadsheets/d/113QZOTS9JQ1WGfwjwQrOpErUBJTAnaBmdjP0_OjA53c/pubhtml?gid=489792061&single=true";
 
 //"data" refers to the column name with no spaces and no capitals
-	//punctuation or numbers in your column name
+//punctuation or numbers in your column name
 //"title" is the column name you want to appear in the published table
-var columns = [ 
-    { "data": "company", "title": "Company" },
-    { "data": "violations", "title": "Claims" },
-    { "data": "total-award", "title": "Total Awarded" }];
+var columns = [{
+  "data": "company",
+  "title": "Company"
+}, {
+  "data": "violations",
+  "title": "Claims"
+}, {
+  "data": "total-award",
+  "title": "Total Awarded"
+}];
 
 $(document).ready(function() {
 
-    function initializeTabletopObject(){
+  function initializeTabletopObject() {
     Tabletop.init({
-        key: key,
-        callback: function(data, tabletop) { 
-            writeTable(data); //call up datatables function
-            }, 
-        simpleSheet: true,
-        debug: false
+      key: key,
+      callback: function(data, tabletop) {
+        writeTable(data); //call up datatables function
+      },
+      simpleSheet: true,
+      debug: false
     });
-}
+  }
 
-    initializeTabletopObject();
+  initializeTabletopObject();
 
-    function writeTable(data){
-        //select main div and put a table there
-		//use bootstrap css to customize table style: http://getbootstrap.com/css/#tables 
-        $('#graphic').html('<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-condensed" id="mySelection"></table>');
+  function writeTable(data) {
+    //select main div and put a table there
+    //use bootstrap css to customize table style: http://getbootstrap.com/css/#tables
+    $('#graphic').html(
+      '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-condensed table-responsive" id="mySelection"></table>'
+    );
 
-        //initilize the DataTable object and put settings in
-        $("#mySelection").DataTable({
-            "data": data,
-            "columns": columns, 
-            "order":[[2, "desc"]], //order on 1st column
-            "pagingType": "simple" //no page numbers
-			//uncomment these options to simplify your table
-			//"paging": false,
-			//"searching": false,
-			//"info": false
-            });
-        }
+    //initilize the DataTable object and put settings in
+    $("#mySelection").DataTable({
+      "autoWidth": false,
+      "data": data,
+      "columns": columns,
+      "order": [
+        [2, "desc"]
+      ], //order on 1st column
+      "pagingType": "simple" //no page numbers
+        //uncomment these options to simplify your table
+        //"paging": false,
+        //"searching": false,
+        //"info": false
+    });
+  }
 });
 //end of writeTable
-
-
-
